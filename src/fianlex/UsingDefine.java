6ba54d5b30377943378f444c1163f7b9364f7e69
woozink <k0109974@naver.com>
2022-01-26 17:31:30 +0900

    finalex

---
+++ src/fianlex/UsingDefine.java
@@ -0,0 +1,12 @@
+package fianlex;
+
+public class UsingDefine {
+    public static void main(String[] args) {
+        System.out.println(Define.GOOD_MORNING);
+        System.out.println("최소" + Define.MIN + " 입니다.");
+        System.out.println("최대" + Define.MAX + " 입니다.");
+        System.out.println("수학과목은" + Define.MATH + " 입니다.");
+        System.out.println("영어과목은" + Define.ENG + " 입니다.");
+
+    }
+}
