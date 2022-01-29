ccb4f434d2bf53674c52cbc5208680bb97f8cd40
woozink <k0109974@naver.com>
2022-01-29 15:29:48 +0900

    Basic class , java.lang

---
+++ src/object/ToString.java
@@ -0,0 +1,27 @@
+package object;
+
+class Book {
+    int bookNumber;
+    String bookTitle;
+
+    Book(int bookNumber, String bookTitle) {
+        this.bookNumber = bookNumber;
+        this.bookTitle = bookTitle;
+
+    }
+
+    @Override
+    public String toString(){
+        return bookTitle + "," + bookNumber;
+    }
+}
+
+public class ToString {
+    public static void main(String[] args) {
+        Book book1 = new Book(200, "개미");
+
+        System.out.println(book1);
+        System.out.println(book1.toString());
+    }
+}
+
