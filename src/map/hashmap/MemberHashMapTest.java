351e357dbf485239ebbf5f7ee078433c53bf7b33
woozink <k0109974@naver.com>
2022-02-01 14:11:13 +0900

    map 이해하기

---
+++ src/map/hashmap/MemberHashMapTest.java
@@ -0,0 +1,25 @@
+//package map.hashmap;
+//
+//import collection.Member;
+//
+//public class MemberHashMapTest {
+//    public static void main(String[] args) {
+//        MemberhashMap memberhashMap = new MemberhashMap();
+//
+//        Member memberLee = new Member(1001, "이지원");
+//        Member memberSon = new Member(1002, "손흥민");
+//        Member memberPark = new Member(1003, "박지성");
+//        Member memberHong = new Member(1004, "홍록기");
+//
+//        MemberhashMap.addMember(memberHong);
+//        MemberhashMap.addMember(memberLee);
+//        MemberhashMap.addMember(memberSon);
+//        MemberhashMap.addMember(memberPark);
+//
+//        memberhashMap.showAllMember();
+//
+//        memberhashMap.removeMember();
+//        memberhashMap.showAllMember();
+//
+//    }
+//}
---
+++ src/map/hashmap/MemberhashMap.java
@@ -0,0 +1,38 @@
+//package map.hashmap;
+//
+//import collection.Member;
+//
+//import java.util.HashMap;
+//import java.util.Iterator;
+//
+//public class MemberhashMap {
+//    private HashMap<Integer, Member> hashMap;
+//
+//    public MemberhashMap() {
+//        hashMap = new hashMap<Integer, Member>();
+//
+//    }
+//
+//    public void addMember(Member member) {
+//        hashMap.put(member.getMemberId(), member);
+//    }
+//
+//    public boolean removeMember(int memberId) {
+//        if (hashMap.containsKey(memberId)) {
+//            hashMap.remove(memberId);
+//            return true;
+//        }
+//        System.out.println(memberId + "가 존재하지 않습니다.");
+//        return false;
+//    }
+//    public void showAllMember(){
+//        Iterator<Integer> ir = hashMap.keySet().iterator();
+//        while (ir.hasNext()){
+//            int key = ir.next();
+//            Member member = hashMap.get(key);
+//            System.out.println(member);
+//        }
+//        System.out.println();
+//    }
+//}
+//
